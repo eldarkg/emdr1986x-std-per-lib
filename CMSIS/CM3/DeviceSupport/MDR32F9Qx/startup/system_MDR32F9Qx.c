da0406c51d72aa2bc68aec1ca75ae0baf8f750b7
Eldar Khayrullin <eldar.khayrullin@mail.ru>
2017-05-20 18:04:45 +0300

    Add option to no reset VTOR register in SystemInit
    Define MDR_NO_RST_VTOR to no reset VTOR register in SystemInit.
    Else VTOR register is reset to default value.

--- CMSIS/CM3/DeviceSupport/MDR32F9Qx/startup/system_MDR32F9Qx.c
+++ CMSIS/CM3/DeviceSupport/MDR32F9Qx/startup/system_MDR32F9Qx.c
@@ -140,11 +140,11 @@ void SystemCoreClockUpdate (void)
   */
 void SystemInit (void)
 {
-	
 #ifndef MDR_NO_RST_VTOR
-  /* Reset the RST clock configuration to the default reset state */
   SCB->VTOR = 0x08000000;
 #endif
+	
+  /* Reset the RST clock configuration to the default reset state */
 
   /* Reset all clock but RST_CLK & BKP_CLK bits */
   MDR_RST_CLK->PER_CLOCK   = (uint32_t)0x8000010;
